9450bcb03bf258fdfa52b59584dc6e969da0467c
mutiara126 <salsamutiara1812@gmail.com>
2024-10-16 22:16:42 +0700

    untuk

---
+++ app/src/main/java/com/example/prakpam2/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.prakpam2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.prakpam2.ui.theme.PrakPAM2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PrakPAM2Theme {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        BasicLayout(modifier = Modifier.padding(innerPadding))
+    }
+   }
+  }
+ }
+}
+@Composable
+fun BasicLayout(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "Login",
+            style = TextStyle(fontSize = 50.sp, fontWeight = FontWeight.Bold)
+
+
+
+
+
+
